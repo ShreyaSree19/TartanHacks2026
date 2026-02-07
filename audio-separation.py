58f7308cfa236eba913b42b8d1752b7e0393437e
brancoc <22brancoc@gmail.com>
2026-02-06 21:45:35 -0500

    update audio-sep file

--- audio-separation.py
+++ audio-separation.py
@@ -1,16 +1,29 @@
 import subprocess
 
-print("Url:", end = " ")
-url = input()
 
-cmd = [
-    "lib/yt-dlp",
-    "-q",
-    "--no-warnings",
-    "-x",
-    "--audio-format", "mp3",
-    "-o", "./downloads/%(title)s.%(ext)s",
-    url
-]
+# might have to run "chmod +x yt-dlp" in lib/ to make sure it is an executable
+# then check "./yt-dlp --version"
+def url_to_mp3():
+    print("Url:", end = " ")
+    url = input()
 
-subprocess.run(cmd, check=True)
+    cmd = [
+        "lib/yt-dlp",
+        "-q",
+        "--no-warnings",
+        "-x",
+        "--audio-format", "mp3",
+        "-o", "./mp3-files/%(title)s.%(ext)s",
+        url
+    ]
+
+    subprocess.run(cmd, check=True)
+
+def mp3_to_wav():
+    pass
+
+def main():
+    url_to_mp3()
+
+if __name__ == "__main__":
+    main()
